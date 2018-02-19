34c25292bc8b9a6435d7d29cf94bad44a5bc9519
sergdel <centaous@protonmail.com>
2018-02-19 22:15:53 +0000

    eslint

--- src/App.js
+++ src/App.js
@@ -30,213 +30,202 @@ if (valid) console.log("Valid!");
 else console.log("Invalid: " + ajv.errorsText(validate.errors));
 
 class App extends Component {
-	state = {
-		selectedMenuItem: "General",
-		availableConfig: null,
-		activeItem: "",
-		menuItems: options["config"],
-		invalidConfig: Boolean(valid) === false,
-		userData: {},
-		savedConfig: {},
-		config: options.config,
-		activeItem: ""
-	};
+  state = {
+    selectedMenuItem: "General",
+    availableConfig: null,
+    activeItem: "",
+    menuItems: options["config"],
+    invalidConfig: Boolean(valid) === false,
+    userData: {},
+    savedConfig: {},
+    config: options.config
+  };
 
-	menuSelect = item => {
-		this.setState({
-			selectedMenuItem: item.title
-		});
-		console.log("setting selectedMenuItem to ", item.component);
-	};
+  menuSelect = item => {
+    this.setState({
+      selectedMenuItem: item.title
+    });
+    console.log("setting selectedMenuItem to ", item.component);
+  };
 
-	save = () => {
-		const result = Array.from(document.querySelector("form").elements)
-			.filter(function(item) {
-				return item.name && item.value;
-			})
-			.map(function(item) {
-				return {
-					[item.name]: item.value
-				};
-			});
+  save = () => {
+    const result = Array.from(document.querySelector("form").elements)
+      .filter(function(item) {
+        return item.name && item.value;
+      })
+      .map(function(item) {
+        return {
+          [item.name]: item.value
+        };
+      });
 
-		this.setState({ savedConfig: result });
-	};
+    this.setState({ savedConfig: result });
+  };
 
-	render() {
-		const { menuItems, config, activeItem, selectedMenuItem } = this.state;
+  render() {
+    const { menuItems, config, activeItem, selectedMenuItem } = this.state;
 
-		return (
-			<Responsive
-				as={Container}
-				className="App"
-				style={{ fontFamily: "Poppins" }}
-				width="500"
-			>
-				<DevTools />
+    return (
+      <Container as={Grid} className="App" style={{ fontFamily: "Poppins" }}>
+        <DevTools />
 
-				<pre style={{ color: "white" }}>
-					{JSON.stringify(this.state.savedConfig)}
-				</pre>
+        <pre style={{ color: "white" }}>
+          {JSON.stringify(this.state.savedConfig)}
+        </pre>
 
-				<Modal open={this.state.invalidConfig}>
-					<Modal.Header> ERROR </Modal.Header>
-					<Modal.Content image>
-						<Modal.Description>
-							<Header> Malformed Configuration </Header>
-							<p> JSON does not match the supplied schema </p>
-						</Modal.Description>
-					</Modal.Content>
-				</Modal>
+        <Modal open={this.state.invalidConfig}>
+          <Modal.Header> ERROR </Modal.Header>
+          <Modal.Content image>
+            <Modal.Description>
+              <Header> Malformed Configuration </Header>
+              <p> JSON does not match the supplied schema </p>
+            </Modal.Description>
+          </Modal.Content>
+        </Modal>
 
-				<Segment
-					as={Header}
-					attached="top"
-					float="left"
-					textAlign="left"
-					dividing
-					style={{
-						backgroundColor: background,
-						border: "none",
-						color: "white"
-					}}
-				>
-					<Menu>
-						<Menu.Item
-							name="logo"
-							active={activeItem === "editorials"}
-							onClick={this.handleItemClick}
-						>
-							<img src={logo} alt="icon" /> PTFeeder
-						</Menu.Item>
+        <Segment
+          as={Header}
+          attached="top"
+          float="left"
+          textAlign="left"
+          dividing
+          style={{
+            backgroundColor: background,
+            border: "none",
+            color: "white"
+          }}
+        >
+          <Menu>
+            <Menu.Item
+              name="logo"
+              active={activeItem === "editorials"}
+              onClick={this.handleItemClick}
+            >
+              <img src={logo} alt="icon" /> PTFeeder
+            </Menu.Item>
 
-						<Menu.Menu position="right" borderless padded>
-							<Menu.Item
-								name="wiki"
-								active={activeItem === "upcomingEvents"}
-								onClick={this.handleItemClick}
-							>
-								<a
-									href="https://github.com/mehtadone/PTFeeder/wiki"
-									target="_blank"
-								>
-									Wiki
-								</a>
-							</Menu.Item>
+            <Menu.Menu position="right" borderless padded>
+              <Menu.Item
+                name="wiki"
+                active={activeItem === "upcomingEvents"}
+                onClick={this.handleItemClick}
+              >
+                <a
+                  href="https://github.com/mehtadone/PTFeeder/wiki"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Wiki
+                </a>
+              </Menu.Item>
 
-							<Menu.Item
-								name="videos"
-								active={activeItem === "upcomingEvents"}
-								onClick={this.handleItemClick}
-							>
-								<a
-									href="https://github.com/mehtadone/PTFeeder/wiki/Videos"
-									target="_blank"
-								>
-									Videos
-								</a>
-							</Menu.Item>
+              <Menu.Item
+                name="videos"
+                active={activeItem === "upcomingEvents"}
+                onClick={this.handleItemClick}
+              >
+                <a
+                  href="https://github.com/mehtadone/PTFeeder/wiki/Videos"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Videos
+                </a>
+              </Menu.Item>
 
-							<Menu.Item
-								name="support"
-								active={activeItem === "upcomingEvents"}
-								onClick={this.handleItemClick}
-							>
-								<a
-									href="https://github.com/mehtadone/PTFeeder/issues"
-									target="_blank"
-								>
-									Support
-								</a>
-							</Menu.Item>
-						</Menu.Menu>
-					</Menu>
-				</Segment>
+              <Menu.Item
+                name="support"
+                active={activeItem === "upcomingEvents"}
+                onClick={this.handleItemClick}
+              >
+                <a
+                  href="https://github.com/mehtadone/PTFeeder/issues"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Support
+                </a>
+              </Menu.Item>
+            </Menu.Menu>
+          </Menu>
+        </Segment>
 
-				<Grid
-					style={{ fontFamily: "Poppins" }}
-					centered
-					divided="vertically"
-				>
-					<Grid.Row style={{ paddingTop: "30px" }}>
-						<Grid.Column width={4} align="center">
-							<Sticky>
-								<Menu
-									vertical
-									point="right"
-									style={{
-										fontFamily: "Poppins",
-										fontSize: "16px",
-										boxShadow: "none",
-										border: "none",
-										width: "auto",
-										textAlign: "center"
-									}}
-								>
-									{menuItems &&
-										menuItems.map(item => (
-											<Menu.Item
-												name={item.title}
-												key={item.title}
-												onClick={() =>
-													this.menuSelect(item)
-												}
-												style={{
-													color: textColour
-												}}
-											>
-												{item.title}
-											</Menu.Item>
-										))}
-								</Menu>
-							</Sticky>
-						</Grid.Column>
-						<Grid.Column width={5}>
-							<Input
-								icon="search"
-								placeholder="Search..."
-								width={1}
-								transparent
-								fluid
-								small
-								inverted
-								padded={false}
-							/>
+        <Grid style={{ fontFamily: "Poppins" }} centered divided="vertically">
+          <Grid.Row style={{ paddingTop: "30px" }}>
+            <Grid.Column width={4} align="center">
+              <Sticky>
+                <Menu
+                  vertical
+                  point="right"
+                  style={{
+                    fontFamily: "Poppins",
+                    fontSize: "16px",
+                    boxShadow: "none",
+                    border: "none",
+                    width: "auto",
+                    textAlign: "center"
+                  }}
+                >
+                  {menuItems &&
+                    menuItems.map(item => (
+                      <Menu.Item
+                        name={item.title}
+                        key={item.title}
+                        onClick={() => this.menuSelect(item)}
+                        style={{
+                          color: textColour
+                        }}
+                      >
+                        {item.title}
+                      </Menu.Item>
+                    ))}
+                </Menu>
+              </Sticky>
+            </Grid.Column>
+            <Grid.Column width={5}>
+              <Input
+                icon="search"
+                placeholder="Search..."
+                width={1}
+                transparent
+                fluid
+                small
+                inverted
+                padded={false}
+              />
 
-							<Divider />
+              <Divider />
 
-							<Form inverted id="form" action="">
-								{config.map(group => (
-									<Header
-										style={{
-											color: "white",
-											display:
-												selectedMenuItem === group.title
-													? "block"
-													: "none"
-										}}
-									>
-										{group.title}
+              <Form inverted id="form" action="">
+                {config.map(group => (
+                  <Header
+                    style={{
+                      color: "white",
+                      display:
+                        selectedMenuItem === group.title ? "block" : "none"
+                    }}
+                  >
+                    {group.title}
 
-										<ComponentGroup group={group.options} />
-									</Header>
-								))}
-							</Form>
-						</Grid.Column>
-						<Grid.Column width={7}>
-							<Sticky>
-									<Button primary fluid onClick={this.save}>
-										Save all changes
-									</Button>
-							</Sticky>
+                    <ComponentGroup group={group.options} />
+                  </Header>
+                ))}
+              </Form>
+            </Grid.Column>
+            <Grid.Column width={7}>
+              <Sticky>
+                <Button primary fluid onClick={this.save}>
+                  Save all changes
+                </Button>
+              </Sticky>
 
-							<Divider />
-						</Grid.Column>
-					</Grid.Row>
-				</Grid>
-			</Responsive>
-		);
-	}
+              <Divider />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
+    );
+  }
 }
 
 export default observer(App);
