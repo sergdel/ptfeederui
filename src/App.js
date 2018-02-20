b13d7e11c1d9cca8c7be1b2a735c6ddf1b155fe7
sergdel <centaous@protonmail.com>
2018-02-20 00:36:46 +0000

    refactor layout into seperate components - update responsive layout.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from "react";
 import { ComponentGroup } from "./components/ComponentFactory";
 import logo from "./assets/images/logo.png";
-import options from "./config/config.json";
-import { background, textColour } from "./config/constants";
+import { config as options } from "./config/config.json";
+import { background, textColour, foreground } from "./config/constants";
 import "./App.css";
 import Ajv from "ajv";
 import schema from "./config/json.schema.json";
-import DevTools from "mobx-react-devtools";
-import { observer } from "mobx-react";
+// import DevTools from "mobx-react-devtools";
+import { Form, Input, Label } from "semantic-ui-react";
 import {
   Button,
   Grid,
-  Form,
   Menu,
-  Segment,
-  Container,
   Modal,
   Header,
   Divider,
   Sticky,
-  Input
+  Segment
 } from "semantic-ui-react";
 
 const ajv = new Ajv({ allErrors: true });
@@ -29,19 +26,21 @@ const valid = validate(options);
 if (valid) console.log("Valid!");
 else console.log("Invalid: " + ajv.errorsText(validate.errors));
 
-class App extends Component {
+export default class App extends Component {
   state = {
     selectedMenuItem: "General",
     availableConfig: null,
     activeItem: "",
-    menuItems: options["config"],
+    menuItems: options.map(item => {
+      return item.title;
+    }),
     invalidConfig: Boolean(valid) === false,
     userData: {},
     savedConfig: {},
     config: options.config
   };
 
-  menuSelect = item => {
+  onMenuSelect = item => {
     this.setState({
       selectedMenuItem: item.title
     });
@@ -66,166 +65,219 @@ class App extends Component {
     const { menuItems, config, activeItem, selectedMenuItem } = this.state;
 
     return (
-      <Container as={Grid} className="App" style={{ fontFamily: "Poppins" }}>
-        <DevTools />
+      <Grid
+        style={{
+          fontFamily: "Poppins",
+          width: "height: 90vh",
+          overflow: "none"
+        }}
+        middle
+        aligned
+        center
+      >
+        {/* <DevTools /> */}
 
-        <pre style={{ color: "white" }}>
-          {JSON.stringify(this.state.savedConfig)}
-        </pre>
+        {/* HEADER  */}
+        <Grid.Row columns={1}>
+          <Grid.Column>
+            <TopMenu activeItem="none" />
+          </Grid.Column>
+        </Grid.Row>
 
-        <Modal open={this.state.invalidConfig}>
-          <Modal.Header> ERROR </Modal.Header>
-          <Modal.Content image>
-            <Modal.Description>
-              <Header> Malformed Configuration </Header>
-              <p> JSON does not match the supplied schema </p>
-            </Modal.Description>
-          </Modal.Content>
-        </Modal>
-
-        <Segment
-          as={Header}
-          attached="top"
-          float="left"
-          textAlign="left"
-          dividing
-          style={{
-            backgroundColor: background,
-            border: "none",
-            color: "white"
-          }}
+        <Grid.Row
+          columns="equal"
+          stretched
+          centered
+          padded
+          style={{ backgroundColor: background }}
         >
-          <Menu>
-            <Menu.Item
-              name="logo"
-              active={activeItem === "editorials"}
-              onClick={this.handleItemClick}
-            >
-              <img src={logo} alt="icon" /> PTFeeder
-            </Menu.Item>
+          {/* Main Content */}
 
-            <Menu.Menu position="right" borderless padded>
-              <Menu.Item
-                name="wiki"
-                active={activeItem === "upcomingEvents"}
-                onClick={this.handleItemClick}
-              >
-                <a
-                  href="https://github.com/mehtadone/PTFeeder/wiki"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Wiki
-                </a>
-              </Menu.Item>
-
-              <Menu.Item
-                name="videos"
-                active={activeItem === "upcomingEvents"}
-                onClick={this.handleItemClick}
-              >
-                <a
-                  href="https://github.com/mehtadone/PTFeeder/wiki/Videos"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Videos
-                </a>
-              </Menu.Item>
-
-              <Menu.Item
-                name="support"
-                active={activeItem === "upcomingEvents"}
-                onClick={this.handleItemClick}
-              >
-                <a
-                  href="https://github.com/mehtadone/PTFeeder/issues"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Support
-                </a>
-              </Menu.Item>
-            </Menu.Menu>
-          </Menu>
-        </Segment>
-
-        <Grid style={{ fontFamily: "Poppins" }} centered divided="vertically">
-          <Grid.Row style={{ paddingTop: "30px" }}>
-            <Grid.Column width={4} align="center">
-              <Sticky>
-                <Menu
-                  vertical
-                  point="right"
-                  style={{
-                    fontFamily: "Poppins",
-                    fontSize: "16px",
-                    boxShadow: "none",
-                    border: "none",
-                    width: "auto",
-                    textAlign: "center"
-                  }}
-                >
-                  {menuItems &&
-                    menuItems.map(item => (
-                      <Menu.Item
-                        name={item.title}
-                        key={item.title}
-                        onClick={() => this.menuSelect(item)}
-                        style={{
-                          color: textColour
-                        }}
-                      >
-                        {item.title}
-                      </Menu.Item>
-                    ))}
-                </Menu>
-              </Sticky>
-            </Grid.Column>
-            <Grid.Column width={5}>
-              <Input
-                icon="search"
-                placeholder="Search..."
-                width={1}
-                transparent
-                fluid
-                small
-                inverted
-                padded={false}
+          <Grid.Column>
+            <Segment>
+              <LeftNav
+                menuItems={menuItems}
+                selectedMenuItem="General"
+                onMenuSelect={this.onMenuSelect}
               />
-
-              <Divider />
-
-              <Form inverted id="form" action="">
-                {config.map(group => (
-                  <Header
-                    style={{
-                      color: "white",
-                      display:
-                        selectedMenuItem === group.title ? "block" : "none"
-                    }}
-                  >
-                    {group.title}
-
-                    <ComponentGroup group={group.options} />
-                  </Header>
-                ))}
-              </Form>
-            </Grid.Column>
-            <Grid.Column width={7}>
-              <Sticky>
-                <Button primary fluid onClick={this.save}>
-                  Save all changes
-                </Button>
-              </Sticky>
-
-              <Divider />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Container>
+            </Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment>1</Segment>
+            <Segment>2</Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment>1</Segment>
+            <Segment>2</Segment>
+            <Segment>3</Segment>
+          </Grid.Column>
+        </Grid.Row>
+        {/* FOOTER */}
+        <Grid.Row columns={1}>
+          <Grid.Column />
+        </Grid.Row>
+      </Grid>
     );
   }
 }
 
-export default observer(App);
+const SaveButton = () => {
+  return (
+    <Button primary fluid onClick={this.save}>
+      Save all changes
+    </Button>
+  );
+};
+
+const TopMenu = activeItem => {
+  return (
+    <Header
+      style={{
+        backgroundColor: background,
+        border: "none",
+        color: "white",
+        boxShadow: "none"
+      }}
+    >
+      <Menu>
+        <Menu.Item
+          name="logo"
+          active={activeItem === "editorials"}
+          onClick={this.handleItemClick}
+        >
+          <img src={logo} alt="icon" /> PTFeeder
+        </Menu.Item>
+
+        <Menu.Menu position="right" borderless padded>
+          <Menu.Item
+            name="wiki"
+            active={activeItem === "upcomingEvents"}
+            onClick={this.handleItemClick}
+          >
+            <a
+              href="https://github.com/mehtadone/PTFeeder/wiki"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Wiki
+            </a>
+          </Menu.Item>
+
+          <Menu.Item
+            name="videos"
+            active={activeItem === "upcomingEvents"}
+            onClick={this.handleItemClick}
+          >
+            <a
+              href="https://github.com/mehtadone/PTFeeder/wiki/Videos"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Videos
+            </a>
+          </Menu.Item>
+
+          <Menu.Item
+            name="support"
+            active={activeItem === "upcomingEvents"}
+            onClick={this.handleItemClick}
+          >
+            <a
+              href="https://github.com/mehtadone/PTFeeder/issues"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Support
+            </a>
+          </Menu.Item>
+          <Menu.Item>
+            <Label color="grey">version 0.0.1</Label>
+          </Menu.Item>
+        </Menu.Menu>
+      </Menu>
+    </Header>
+  );
+};
+
+const LeftNav = ({ menuItems, selectedMenuItem, onMenuSelect }) => {
+  return (
+    <Grid.Column width={4} align="center">
+      <Sticky>
+        <Menu
+          vertical
+          point="right"
+          style={{
+            fontFamily: "Poppins",
+            fontSize: "16px",
+            boxShadow: "none",
+            border: "none",
+            width: "auto",
+            textAlign: "center"
+          }}
+        >
+          {menuItems &&
+            menuItems.map(title => (
+              <Menu.Item
+                name={title}
+                key={title}
+                onClick={() => onMenuSelect(title)}
+                style={{
+                  color: textColour
+                }}
+              >
+                {title}
+              </Menu.Item>
+            ))}
+        </Menu>
+      </Sticky>
+    </Grid.Column>
+  );
+};
+
+const NavigationMenu = (config, selectedMenuItem) => {
+  return (
+    <Grid.Column width={5}>
+      <Input
+        icon="search"
+        placeholder="Search..."
+        width={1}
+        transparent
+        fluid
+        small
+        inverted
+        padded={false}
+      />
+
+      <Divider />
+
+      <Form inverted id="form" action="">
+        {config.map(group => (
+          <Header
+            style={{
+              color: "white",
+              display: selectedMenuItem === group.title ? "block" : "none"
+            }}
+          >
+            {group.title}
+
+            <ComponentGroup group={group.options} />
+          </Header>
+        ))}
+      </Form>
+    </Grid.Column>
+  );
+};
+
+const ErrorModal = () => {
+  return (
+    <Modal open={this.state.invalidConfig}>
+      <Modal.Header> ERROR </Modal.Header>
+      <Modal.Content image>
+        <Modal.Description>
+          <Header> Malformed Configuration </Header>
+          <p> JSON does not match the supplied schema </p>
+        </Modal.Description>
+      </Modal.Content>
+    </Modal>
+  );
+};
