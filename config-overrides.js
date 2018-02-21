095e8b7e7588694dd45f24b0b255e873fc6fbb35
sergdel <centaous@protonmail.com>
2018-02-21 00:40:28 +0000

    fix rewire babel emotion

--- config-overrides.js
+++ config-overrides.js
@@ -4,15 +4,29 @@ const { injectBabelPlugin } = require("react-app-rewired");
 
 module.exports = function override(config, env) {
   //do stuff with the webpack config...
-  config = injectBabelPlugin("emotion/babel", config);
 
-  // use the Preact rewire
+  const babelLoader = function(conf) {
+    if (!conf.loader) return false;
+    return conf.loader.indexOf("babel-loader") > -1;
+  };
+
+  function rewireEmotion(config, env) {
+    const babelrc = config.module.rules.find(babelLoader).options;
+    babelrc.plugins = ["emotion/babel"].concat(babelrc.plugins || []);
+
+    return config;
+  }
+
+  module.exports = function override(config, env) {
+    config = rewireEmotion(config, env);
+    return config;
+  };
+
   if (env === "production") {
     console.log("⚡ Production build with Preact");
     config = rewirePreact(config, env);
   }
 
-  // use the MobX rewire
   config = rewireMobX(config, env);
 
   return config;
