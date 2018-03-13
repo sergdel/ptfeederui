f75fee59d52c91245f35863b74ed3c4051e99a48
sergdel <centaous@protonmail.com>
2018-03-13 23:29:44 +0000

    extract store strings to constants

--- src/app/constants/stores.ts
+++ src/app/constants/stores.ts
@@ -1,1 +1,4 @@
-export const STORE_ROUTER = 'router';
+export const STORE_ROUTER = "router";
+export const APP_SETTINGS = "appSettings";
+export const SETTINGS = "settings";
+export const UI_DEFS = "componentDefinitions";
