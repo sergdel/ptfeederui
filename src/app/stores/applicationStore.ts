22b171766dab5ae1c11fe8e322a15b8c8a8bc875
sergdel <centaous@protonmail.com>
2018-03-21 22:38:11 +0000

    protect trees from outside modification

--- src/app/stores/applicationStore.ts
+++ src/app/stores/applicationStore.ts
@@ -1,9 +1,11 @@
 import { ApplicationModel } from "./models";
+import { protect } from "mobx-state-tree";
 export class ApplicationStore {
   rootStore;
   store;
   constructor(rootStore) {
     this.rootStore = rootStore;
     this.store = ApplicationModel.create();
+    protect(this.store);
   }
 }
--- src/app/stores/componentDefinitions.ts
+++ src/app/stores/componentDefinitions.ts
@@ -1,10 +1,12 @@
 import { ComponentModel } from "./models/Components";
 import UIConfig from "../config/config.json";
+import { protect } from "mobx-state-tree";
 export class ComponentStore {
   rootStore;
   store;
   constructor(rootStore) {
     this.rootStore = rootStore;
     this.store = ComponentModel.create(UIConfig);
+    protect(this.store);
   }
 }
