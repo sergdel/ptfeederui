5a08979032379fb9f4b9f79264f8374bc94dbf96
sergdel <centaous@protonmail.com>
2018-03-23 08:56:53 +0000

    prevent accidentally opening wiki

--- src/app/components/InfoLabel.tsx
+++ src/app/components/InfoLabel.tsx
@@ -2,11 +2,12 @@ import * as React from "react";
 import { Label, Popup } from "semantic-ui-react";
 
 export const InfoLabel: React.SFC<any> = ({ title, wiki }) => {
-  return (
+  return title ? (
     <Popup
       trigger={
         <Label
           onClick={() =>
+            wiki &&
             window.open("https://github.com/mehtadone/PTFeeder/wiki", "_blank")
           }
         >
@@ -18,11 +19,9 @@ export const InfoLabel: React.SFC<any> = ({ title, wiki }) => {
         <a href={"https://github.com/mehtadone/PTFeeder" + wiki}>
           {"https://github.com/mehtadone/PTFeeder" + wiki}
         </a>
-      ) : (
-        "more info"
-      )}
+      ) : null}
     </Popup>
-  );
+  ) : null;
 };
 
 export default InfoLabel;
